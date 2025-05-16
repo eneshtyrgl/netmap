638cc47db7a2e0d496aec76a1e3b21169d95d723
eneshtyrgl <eneshtyrgl@gmail.com>
2025-05-17 00:29:14 +0300

    service created

---
+++ netmap-service/src/main/java/com/netmap/netmapservice/NetmapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.netmap.netmapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetmapServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetmapServiceApplication.class, args);
+    }
+
+}
---
+++ netmap-service/src/test/java/com/netmap/netmapservice/NetmapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netmap.netmapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetmapServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
