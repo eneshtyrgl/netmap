e7fb5a65185036cf4addcd8e76ebe91abc92aa6e
eneshtyrgl <eneshtyrgl@gmail.com>
2025-05-17 20:26:05 +0300

    add derived query methods to repositories

--- netmap-service/src/main/java/com/netmap/netmapservice/repository/JobApplicationRepository.java
+++ netmap-service/src/main/java/com/netmap/netmapservice/repository/JobApplicationRepository.java
@@ -1,11 +1,15 @@
 package com.netmap.netmapservice.repository;
 
 import com.netmap.netmapservice.model.JobApplication;
+import com.netmap.netmapservice.model.JobPosting;
+import com.netmap.netmapservice.model.JobSeeker;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
-@Repository
 public interface JobApplicationRepository extends JpaRepository<JobApplication, UUID> {
+    boolean existsByJobSeekerAndJobPosting(JobSeeker jobSeeker, JobPosting jobPosting);
+    List<JobApplication> findByJobSeeker(JobSeeker jobSeeker);
+    List<JobApplication> findByJobPosting(JobPosting jobPosting);
 }
--- netmap-service/src/main/java/com/netmap/netmapservice/repository/SkillRepository.java
+++ netmap-service/src/main/java/com/netmap/netmapservice/repository/SkillRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+import java.util.List;
 
-@Repository
 public interface SkillRepository extends JpaRepository<Skill, UUID> {
+    List<Skill> findByNameIn(List<String> names);
 }
