afe5cc047effecf06f198c46c7f434649025b89d
eneshtyrgl <eneshtyrgl@gmail.com>
2025-05-18 23:51:39 +0300

    configure routing and root application structure

--- netmap-gateway/src/app/app.routes.ts
+++ netmap-gateway/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import {MapComponent} from './features/map/map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'map', component: MapComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
